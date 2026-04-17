7a2eab8dd5872fd9cee4cdd61e1c7c84f106264c
Fubu Linux <brendonangelo@hotmail.com>
2026-04-17 00:50:13 -0300

    reset migrations

--- back/prisma/migrations/20260417033509_first_migration/migration.sql
+++ back/prisma/migrations/20260417033509_first_migration/migration.sql
@@ -4,6 +4,9 @@ CREATE TYPE "AssetClassType" AS ENUM ('STOCK', 'CRYPTO', 'FII', 'FIXED_INCOME', 
 -- CreateEnum
 CREATE TYPE "MarketType" AS ENUM ('BRAZIL', 'INTERNATIONAL', 'BDR');
 
+-- CreateEnum
+CREATE TYPE "ExchangeCodeType" AS ENUM ('BZ', 'US', 'CRYPTO', 'FIXED');
+
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
@@ -23,7 +26,7 @@ CREATE TABLE "Investment" (
     "market" "MarketType" NOT NULL,
     "assetClass" "AssetClassType" NOT NULL,
     "name" TEXT NOT NULL,
-    "exchangeCode" TEXT NOT NULL DEFAULT 'UNKNOWN',
+    "exchangeCode" "ExchangeCodeType" NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -48,10 +51,10 @@ CREATE TABLE "UserInvestment" (
 CREATE UNIQUE INDEX "User_phoneNumber_key" ON "User"("phoneNumber");
 
 -- CreateIndex
-CREATE INDEX "User_phoneNumber_idx" ON "User"("phoneNumber");
+CREATE UNIQUE INDEX "Investment_ticker_exchangeCode_key" ON "Investment"("ticker", "exchangeCode");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Investment_ticker_exchangeCode_key" ON "Investment"("ticker", "exchangeCode");
+CREATE INDEX "UserInvestment_userId_idx" ON "UserInvestment"("userId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "UserInvestment_userId_investmentId_key" ON "UserInvestment"("userId", "investmentId");
